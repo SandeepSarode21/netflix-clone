299579f38a902159df9e02565590b2d2be390f73
Sandeep Sarode <sandeepsarode21@gmail.com>
2021-09-29 11:53:06 -0400

    update url

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const APIKEY = "5093886aa3e0b82715f209700fe0eba3";
 
 const requests = {
   fetchTreanding: `trending/all/day?api_key=${APIKEY}&language=en-US`,
-  fetchNetflixOriginal: `trending/all/day?api_key=${APIKEY}&language=en-US`,
+  fetchNetflixOriginal: `movie/trending/all/day?api_key=${APIKEY}&language=en-US`,
   fetchTopRated: `movie/top_rated?api_key=${APIKEY}&language=en-US`,
   fetchAction: `discover/movie?api_key=${APIKEY}&language=en-US&include_adult=false&with_genres=28`,
   fetchHorror: `discover/movie?api_key=${APIKEY}&language=en-US&include_adult=false&with_genres=27`,
